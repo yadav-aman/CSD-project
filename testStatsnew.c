31d5febbc7b60ce3a42ccc872ed2769ad20d765c
NamanRastogi888 <62835998+NamanRastogi888@users.noreply.github.com>
2020-05-07 19:16:24 +0530

    Update testStatsnew.c

--- testStatsnew.c
+++ testStatsnew.c
@@ -7,7 +7,7 @@
  insertCandidate(&rootC,"NOTA",".",".",".",2);
  insertCandidate(&rootC,"Pushpam Priya Choudhary","Plurals","Flying Horse","F",10);
  insertCandidate(&rootC,"Ram Vilas Paswan","Lok Janashakti Party","RGB Flag","M",12);
- insertCandidate(&rootC,"Tejasvi Yadav","Rashtriya Janta Dal","Lantern","M",12);
- insertCandidate(&rootC,"Nitish Kumar","Janta Dal(United)","Arrow","M",8);
+ insertCandidate(&rootC,"Tejaswi Yadav","Rashtriya Janata Dal","Lantern","M",12);
+ insertCandidate(&rootC,"Nitish Kumar","Janata Dal(United)","Arrow","M",8);
  electionStatistics(&rootC,44);
  }
